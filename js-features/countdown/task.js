c04edf69c81b36faaa930cbcff368f9cb953805e
unker <kfi.unknown@gmail.com>
2023-02-10 00:07:03 +0300

    format timer

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,19 @@
 const timer = document.getElementById('timer');
 let timerValue = Number(timer.textContent);
+let date = new Date(null);
 
+setTextTimer(timerValue);
+
+function setTextTimer(timerValue) {
+    date.setSeconds(timerValue);
+    timer.textContent = date.toISOString().slice(11, 19);
+}
 
 let refreshIntervalId = setInterval(() => {
     timerValue--;
-    timer.textContent = timerValue;
 
-    if(timerValue <= 0) {
+    setTextTimer(timerValue)
+    if (timerValue <= 0) {
         clearInterval(refreshIntervalId);
         window.alert('Вы победили в конкурсе!');
     }
