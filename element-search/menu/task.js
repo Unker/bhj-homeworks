41c4787d91f9d068d9f259cda2775ae6149e9753
unker <kfi.unknown@gmail.com>
2023-02-16 14:33:56 +0300

    menu task done

---
+++ element-search/menu/task.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('.menu__link');
+let menus = document.querySelectorAll('.menu');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    const subMenu = link.nextElementSibling;
+    if (subMenu && subMenu.classList.contains('menu_sub')) {
+        if(subMenu.classList.contains('menu_active')) {
+            subMenu.classList.remove('menu_active');
+        } else {
+            menus.forEach(menu => {
+                menu.classList.remove('menu_active');
+            });
+            subMenu.classList.add('menu_active');
+        }
+        e.preventDefault();
+    } 
+  });
+});
