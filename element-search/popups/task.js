01d3ba90d17036acc334985be5460f8b5ef39eac
unker <kfi.unknown@gmail.com>
2023-02-16 13:17:48 +0300

    popups task done

---
+++ element-search/popups/task.js
@@ -0,0 +1,20 @@
+const modal_main = document.getElementById('modal_main')
+const modal_success = document.getElementById('modal_success')
+const modals_close = document.querySelectorAll('.modal__close_times');
+const show_success = document.querySelector('.show-success')
+
+
+modal_main.classList.add("modal_active");
+
+modals_close.forEach(modal_close => {
+    modal_close.onclick = (element) => {
+        modal = element.srcElement.closest('.modal');
+        modal.classList.remove("modal_active");   
+    }
+});
+
+show_success.onclick = (element) => {
+    console.log('asd');
+    modal_main.classList.remove("modal_active"); 
+    modal_success.classList.add("modal_active");
+}
