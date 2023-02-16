59fd4a7bbb4bad33aa3e6572b1f5810e40c21c50
unker <kfi.unknown@gmail.com>
2023-02-16 18:25:41 +0300

    camelCase

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,19 +1,19 @@
-const modal_main = document.getElementById('modal_main')
-const modal_success = document.getElementById('modal_success')
-const modals_close = document.querySelectorAll('.modal__close_times');
-const show_success = document.querySelector('.show-success')
+const modalMain = document.getElementById('modal_main')
+const modalSuccess = document.getElementById('modal_success')
+const modalsClose = document.querySelectorAll('.modal__close_times');
+const showSuccess = document.querySelector('.show-success')
 
 
-modal_main.classList.add("modal_active");
+modalMain.classList.add("modal_active");
 
-modals_close.forEach(modal_close => {
+modalsClose.forEach(modal_close => {
     modal_close.onclick = (element) => {
         modal = element.srcElement.closest('.modal');
         modal.classList.remove("modal_active");
     }
 });
 
-show_success.onclick = (element) => {
-    modal_main.classList.remove("modal_active");
-    modal_success.classList.add("modal_active");
+showSuccess.onclick = (element) => {
+    modalMain.classList.remove("modal_active");
+    modalSuccess.classList.add("modal_active");
 }
