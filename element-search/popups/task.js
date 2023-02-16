43b455deb875baeb1a9ba2458f5d7cdfeff115cc
unker <kfi.unknown@gmail.com>
2023-02-16 16:38:55 +0300

    del log

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -9,12 +9,11 @@ modal_main.classList.add("modal_active");
 modals_close.forEach(modal_close => {
     modal_close.onclick = (element) => {
         modal = element.srcElement.closest('.modal');
-        modal.classList.remove("modal_active");   
+        modal.classList.remove("modal_active");
     }
 });
 
 show_success.onclick = (element) => {
-    console.log('asd');
-    modal_main.classList.remove("modal_active"); 
+    modal_main.classList.remove("modal_active");
     modal_success.classList.add("modal_active");
 }
