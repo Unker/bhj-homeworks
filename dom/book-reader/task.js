21bb31f297dc090911f54e5e75a0cfe6a3c5192f
unker <kfi.unknown@gmail.com>
2023-02-24 17:42:07 +0300

    sens all controllers

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,6 +1,7 @@
 // кнопки управления
 const bookControlers = document.querySelectorAll('.book__controls');
 let font = {};
+let bg = {};
 
 bookControlers.forEach((bookControler) => {
     bookControler.addEventListener('click', (e) => {
@@ -8,12 +9,26 @@ bookControlers.forEach((bookControler) => {
         const book = e.currentTarget.parentNode;
         console.log(book)
         target = e.target;
+        // найти book__control
+        const control = target.parentNode;
+        console.log(control)
 
-        if(target.className.includes('font-size')) {
+        if(control.className.includes('book__control_font-size')) {
             font['size'] = e.target.dataset.size;
             changeFont(book, font);
+            // смена активного шрифта
+            fontSize = control.querySelectorAll('.font-size');
+            fontSize.forEach((item) => item.classList.remove('font-size_active'));
+            
+        } else if(control.className.includes('book__control_color')) {
+            font['color'] = e.target.dataset.textColor;
+            changeFont(book, font);
+        }  else if(control.className.includes('book__control_background')) {
+            bg['color'] = e.target.dataset.bgColor;
+            changeBackgroud(book, bg);
         }
 
+        console.log(book.classList)
     });
 });
 
@@ -30,4 +45,38 @@ function changeFont(book, font) {
         default:
             break;
     }
+
+    color = font['color']
+    switch(color) {
+        case 'black':
+            book.classList.add('book_color-black');
+            break;
+        case 'gray':
+            book.classList.add('book_color-gray');
+            break;
+        case 'whitesmoke':
+            book.classList.add('book_color-whitesmoke');
+            break;
+        default:
+            break;
+    }
+}
+
+function changeBackgroud(book, font) {
+    book.classList = ['book'];
+    color = bg['color']
+    switch(color) {
+        case 'black':
+            book.classList.add('bg_color_black');
+            break;
+        case 'gray':
+            book.classList.add('bg_color_gray');
+            break;
+        case 'white':
+            book.classList.add('bg_color_white');
+            break;
+        default:
+            break;
+    }
+
 }
