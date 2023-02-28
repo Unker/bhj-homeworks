c9b12641f248d0078ce78a2a5277d79d25933552
unker <kfi.unknown@gmail.com>
2023-02-28 23:17:40 +0300

    add random response

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -6,23 +6,25 @@ const messages = document.querySelector( '.chat-widget__messages' );
 chatBtn.addEventListener('click', (e) => chat.classList.add('chat-widget_active'));
 
 input.addEventListener('keydown', (e) => {
-    console.log(e.key);
     if(e.key === 'Enter' && e.repeat === false) {
         if(input.value) {
             // отправим сообщение и очистим поле ввода
-            sendMsg(input.value);
+            sendMsg(input.value, true);
             input.value = '';
             
             // отправим ответ на принятое сообщение от пользователя
-            responseMsg();
+            responseRndMsg();
+
+            // прокрутку окна чата до блока последнего комментария
+
         }
     }
 });
 
 // отправить сообщение в чат
-function sendMsg(msg) {
+function sendMsg(msg, isClient = false) {
     messages.innerHTML += `
-    <div class="message">
+    <div class="message ${isClient?'message_client':''}">
       <div class="message__time">${getCurrentTime()}</div>
       <div class="message__text">${msg}</div>
     </div>
@@ -35,4 +37,33 @@ function getCurrentTime() {
     return date.toLocaleTimeString();
 }
 
+// отправить случайный ответ пользователы
+function responseRndMsg() {
+    let msg = sentence()
+    sendMsg(msg);
+}
 
+var verbs, nouns, adjectives, adverbs, preposition;
+nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"];
+verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"];
+adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
+adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+
+function randGen() {
+  return Math.floor(Math.random() * 5);
+}
+
+function sentence() {
+  var rand1 = Math.floor(Math.random() * 10);
+  var rand2 = Math.floor(Math.random() * 10);
+  var rand3 = Math.floor(Math.random() * 10);
+  var rand4 = Math.floor(Math.random() * 10);
+  var rand5 = Math.floor(Math.random() * 10);
+  var rand6 = Math.floor(Math.random() * 10);
+  //                var randCol = [rand1,rand2,rand3,rand4,rand5];
+  //                var i = randGen();
+  var content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
+
+  return content;
+};
