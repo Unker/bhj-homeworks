dd9c4bc4db60f14809bf783b948126f67c4e3623
unker <kfi.unknown@gmail.com>
2023-03-02 23:35:51 +0300

    checkbox indeterminated

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,31 +1,32 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]')
 
-checkboxes.forEach( (checkbox) => {
-  checkbox.addEventListener('change', (event) => checkboxDetermination(event.target));
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (event) => checkboxDetermination(event.target));
 });
 
-function checkboxDetermination( initiator ) {
-  console.log(initiator.closest('.interest'))
-  let children = initiator.closest('.interest').querySelectorAll('input[type="checkbox"]');
-  console.log(children)
-  
-  children.forEach((child) => {
-    child.checked = initiator.checked;
-    child.indeterminate = false;
-  });
-//   indeterminateWalk();
+function checkboxDetermination(initiator) {
+    let children = initiator.closest('.interest').querySelectorAll('input[type="checkbox"]');
+
+    children.forEach((child) => {
+        child.checked = initiator.checked;
+        child.indeterminate = false;
+    });
+    indeterminateWalk();
 }
 
-// function indeterminateWalk() {
-//   [...checkboxes]
-//     .forEach( checkbox => {
-//       let checkboxChildren = document.querySelectorAll(`[name^="${checkbox.name}-"]`);
-    
-//       if( checkboxChildren.length == 0 ) return;
-    
-//       let unCheckedChildren = [...checkboxChildren].filter( child => !child.checked );
-    
-//       checkbox.indeterminate = ( unCheckedChildren.length > 0 && unCheckedChildren.length < checkboxChildren.length);
-//       checkbox.checked = ( unCheckedChildren.length === 0 );
-//   });
-// }
+function indeterminateWalk() {
+    [...checkboxes]
+        .forEach(checkbox => {
+            let checkboxChildren = checkbox.closest('.interest').querySelectorAll('input[type="checkbox"]');
+
+            // remove self node
+            checkboxChildren = [].slice.call(checkboxChildren, 1);
+
+            if (checkboxChildren.length == 0) return;
+
+            let unCheckedChildren = [...checkboxChildren].filter(child => !child.checked);
+            checkbox.indeterminate = (unCheckedChildren.length > 0 && unCheckedChildren.length < checkboxChildren.length);
+
+            checkbox.checked = (unCheckedChildren.length === 0);
+        });
+}
