7cc715843f8d04ccee06a8e30f06c17c06405229
Кузнецов Ф.И <fkuznetsov@nkbvs.ttn.ru>
2023-03-08 12:30:02 +0300

    autocreate element Tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,23 +1,44 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
-const tooltip = document.querySelector('.tooltip');
+//const tooltip = document.querySelector('.tooltip');
 
 hasTooltip.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		e.preventDefault();
-		showTooltip(e.target);
+		deleteTooltip();
+		let tooltip = createTooltip(e.target);
+		showTooltip(e.target, tooltip);
 	});
 });
 
 document.addEventListener('click', (e) => {
 	if(!e.target.classList.contains('has-tooltip')) {
+		const tooltip = document.querySelector('.tooltip');
 		tooltip.classList.remove('tooltip_active');
 	}
 });
 
+function deleteTooltip() {
+	const tooltip = document.querySelector('.tooltip');
+	if(tooltip) {
+		tooltip.remove();
+	}
+}
+
 //document.addEventListener('scroll', () => setCoordTolltip());
+// добавление элемента в DOM дерево
+function createTooltip (el) {
+	var tooltip = document.createElement('div');
+	tooltip.classList.add('tooltip');
+	el.style.left = '0px';
+	el.style.top = '0px';
+	if (el) {
+		el.insertAdjacentElement('afterEnd', tooltip);
+	}
+	return tooltip
+}
 
 // отобразить подсказку
-function showTooltip(el) {	
+function showTooltip(el, tooltip) {	
 	let coords = getCoords(el);
 	
 	tooltip.classList.add('tooltip_active');
