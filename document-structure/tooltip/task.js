3679809341b7fc7973ac55a7d456e68e7602524a
Кузнецов Ф.И <fkuznetsov@nkbvs.ttn.ru>
2023-03-09 16:49:14 +0300

    positon tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,8 +7,8 @@ hasTooltip.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		e.preventDefault();
 		hideTooltip(tooltip);
-		e.target.appendChild(tooltip);
-		showTooltip(e.target, tooltip);
+		e.target.parentNode.insertBefore(tooltip, e.target.nextSibling);
+		showTooltip(e.target, tooltip, 'right');
 	});
 });
 
@@ -26,13 +26,51 @@ function hideTooltip(tooltip) {
 };
 
 // отобразить подсказку
-function showTooltip(el, tooltip) {	
+function showTooltip(el, tooltip, position='bottom') {	
 	let coords = getCoords(el);
 	
 	tooltip.classList.add('tooltip_active');
-	tooltip.style.left = coords.left + 'px';
-	tooltip.style.top = coords.bottom + 'px';
 	tooltip.innerHTML = el.title;
+	let shift = {x:0, y:0};	
+	switch(position) {
+		case 'right':
+			// если подсказка может уместиться справа
+			if(coords.right + tooltip.offsetWidth < window.innerWidth) {
+				shift.y = -(coords.height + tooltip.offsetHeight)/2;
+				shift.x =  coords.width;
+				console.log('right')
+				break;
+			}
+		case 'left':
+			// если подсказка может уместиться слева
+			if(coords.left - tooltip.offsetWidth > 0) {
+				shift.y = -(coords.height + tooltip.offsetHeight)/2;
+				shift.x = -(tooltip.offsetWidth);
+				console.log('left')
+				break;
+			}
+		case 'bottom':
+			// если подсказка может уместиться снизу
+			if(coords.y + coords.height + tooltip.offsetHeight < window.innerHeight) {
+				console.log('bottom')
+				break;
+			}
+		case 'top':
+			// если подсказка может уместиться сверху
+			if(coords.y - coords.height - tooltip.offsetHeight > 0) {
+				console.log('top');
+				shift.y = -(coords.height + tooltip.offsetHeight);
+				break;
+			}
+			console.log('top')
+			break;
+		default:
+			// bottom
+			break;
+			
+	}
+	tooltip.style.left = coords.left + shift.x + 'px';
+	tooltip.style.top = coords.bottom + shift.y + 'px';
 
 };
 
@@ -45,5 +83,9 @@ function getCoords(el) {
 		right: box.right + window.pageXOffset,
 		bottom: box.bottom + window.pageYOffset,
 		left: box.left + window.pageXOffset,
+		height: box.height,
+		width: box.width,
+		y: box.top,
+		x: box.right,
 	};
 };
