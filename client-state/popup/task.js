fc5dac0cab0c58617a6318d1d552eae1a004ee83
Кузнецов Ф.И <fkuznetsov@nkbvs.ttn.ru>
2023-03-30 18:22:01 +0300

    add cookie

---
+++ client-state/popup/task.js
@@ -0,0 +1,27 @@
+const modalSubs = document.getElementById('subscribe-modal')
+const modalsClose = document.querySelectorAll('.modal__close_times');
+
+// let subscribeIsClosed = JSON.parse(localStorage.getItem('subscribeIsClosed'));
+// console.log(subscribeIsClosed)
+let subscribeIsClosed
+
+let pairs = document.cookie.split('; ');
+console.log(pairs)
+subscribeIsClosed = pairs.find(p => p.startsWith('subscribeIsClosed='));
+if (subscribeIsClosed) {
+    subscribeIsClosed = subscribeIsClosed.substring('subscribeIsClosed='.length);
+}
+
+if(!subscribeIsClosed) {
+    modalSubs.classList.add("modal_active");
+}
+
+modalsClose.forEach(modal_close => {
+    modal_close.onclick = (element) => {
+        modal = element.srcElement.closest('.modal');
+        modal.classList.remove("modal_active");
+        // запишем закрытие окна в хранилище
+        // localStorage.setItem('subscribeIsClosed', JSON.stringify(true));
+        document.cookie = 'test=Hello; subscribeIsClosed=true';
+    }
+});
