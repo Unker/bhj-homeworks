2b0bb29bbfce1fe846a7d5726e91b433e088f457
unker <kfi.unknown@gmail.com>
2023-04-11 21:31:15 +0300

    Убрал таймер. Убрал переменную contentIsChanged

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,26 +2,15 @@ const editor = document.getElementById('editor');
 const clear = document.getElementById('clear_btn');
 
 // считаем задачи  из localStorage
-let text = JSON.parse(localStorage.getItem("text"));
-if(!text) text = '';
-// и отобразим
-editor.value = text
+editor.value = localStorage.getItem("text");
 
-let contentIsChanged = false;
-
-let intervalAutoSave = setInterval(() => {
-    if (contentIsChanged) {
-		//запишем в хранилище
-		localStorage.setItem("text", JSON.stringify(editor.value));
-        contentIsChanged = false;
-    }
-}, 1000)
 
 editor.addEventListener('input', (e) => {
-    contentIsChanged = true;
+    localStorage.setItem("text", editor.value);
 })
+
 
 clear.addEventListener('click', (e) => {
     editor.value = '';
-    contentIsChanged = true;
+    localStorage.removeItem("text");
 });
